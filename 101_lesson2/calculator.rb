cccd09be2ea948a51a330379a7958b89d77a96bc
Alex Sklar <sklarfox@gmail.com>
2022-09-12 14:53:09 -0600

    Updating calculator with bonus features

--- 101_lesson2/calculator.rb
+++ 101_lesson2/calculator.rb
@@ -1,73 +1,76 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts "=> #{message}"
 end
 
 def valid_number?(num)
-  num.to_i != 0
+  valid_integer?(num) || valid_float?(num)
+end
+
+def valid_integer?(num)
+  num == num.to_i.to_s 
+end
+
+def valid_float?(num)
+  num == num.to_f.to_s
 end
 
 def operation_to_message(op)
+  selection = ''
   case op
   when '1'
-    'Adding'
+    selection = 'Adding'
   when '2'
-    'Subtracting'
+    selection = 'Subtracting'
   when '3'
-    'Multiplying'
+    selection = 'Multiplying'
   when '4'
-    'Dividing'
+    selection = 'Dividing'
   end
+  return selection
 end
 
-prompt "=> Welcome to Calculator! Enter your name: "
+prompt (MESSAGES['welcome'])
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt "Make sure to use a valid name."
+    prompt (MESSAGES['name'])
   else
     break
   end
 end
 
-
 loop do # main loop
   digit1 = ''
 
   loop do
-    prompt "What's the first number?"
+    prompt (MESSAGES['first_num'])
     digit1 = gets.chomp
 
     if valid_number?(digit1)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt (MESSAGES['invalid_num'])
     end
   end
 
   digit2 = ''
   loop do
-  prompt "What's the second number?"
-  digit2 = gets.chomp
-
+    prompt (MESSAGES['second_num'])
+    digit2 = gets.chomp
     if valid_number?(digit2)
       break
     else
-      prompt "Hmm... that doesn't look like a valid number"
+      prompt (MESSAGES['invalid_num'])
     end
   end
 
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  prompt operator_prompt
+  prompt (MESSAGES['operator_prompt'])
 
   operator = ''
   loop do
@@ -83,14 +86,14 @@ loop do # main loop
   prompt "#{operation_to_message(operator)} the two numbers..."
 
   result = case operator
-          when '1'
-            digit1.to_i + digit2.to_i
-          when '2'
-            digit1.to_i - digit2.to_i
-          when '3'
-            digit1.to_i * digit2.to_i
-          when '4'
-            digit1.to_f / digit2.to_f
+           when '1'
+             digit1.to_i + digit2.to_i
+           when '2'
+             digit1.to_i - digit2.to_i
+           when '3'
+             digit1.to_i * digit2.to_i
+           when '4'
+             digit1.to_f / digit2.to_f
           end
 
   prompt "The result is #{result}"
