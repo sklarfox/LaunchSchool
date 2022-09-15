4d58a1f6157d7150c938c7c2b73dcd01b90869b2
Alex Sklar <sklarfox@gmail.com>
2022-09-15 12:23:42 -0600

    rps3 implimented scoreboard

--- 101_lesson2/rps3.rb
+++ 101_lesson2/rps3.rb
@@ -14,11 +14,26 @@ def display_result(player, computer)
   winning_table = VALID_CHOICES.rotate(VALID_CHOICES.find_index(player))
 
   if winning_table.find_index(computer) == 0
-    prompt "It's a tie!"
-  elsif winning_table.find_index(computer) == 1
-    prompt "Computer won!"
+    prompt "It's a tie!\n\n"
+  elsif winning_table.find_index(computer) == 1 ||
+        winning_table.find_index(computer) == 3
+    prompt "Computer won the round!\n\n"
   else
-    prompt "You won!"
+    prompt "You won the round!\n\n"
+  end
+end
+
+def return_winner(player, computer)
+  # Rotates the choices table to align with player choice at index 0
+  winning_table = VALID_CHOICES.rotate(VALID_CHOICES.find_index(player))
+
+  if winning_table.find_index(computer) == 0
+    return :ties
+  elsif winning_table.find_index(computer) == 1 ||
+        winning_table.find_index(computer) == 3
+    return :computer
+  else
+    return :player
   end
 end
 
@@ -42,9 +57,21 @@ def standardize_choice(input)
   end
 end
 
-# Main game loop
+def display_score(hash) # This is probably ugly formatting. TODO use the formatted blocky thing
+  prompt "Scoreboard:"
+  print "  | "
+  hash.each { |key, value| print "#{key}: #{value} | " }
+  puts "\n\n"
+end
+
+# Main game, until winner
+system('clear')
+scoreboard = {player: 0, computer: 0, ties: 0}
+
 loop do
+
   choice = ''
+
   loop do   # Collect user input loop
     # TODO display_choices() to include abrv
     prompt "Choose one: #{VALID_CHOICES.join(', ')}"
@@ -52,6 +79,7 @@ loop do
 
     if valid_choice?(choice)
       choice = standardize_choice(choice)  # TODO Cleanup? This is probably ugly code
+      system('clear')
       break
     else
       prompt "That is not a valid choice."
@@ -60,14 +88,18 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  prompt "You chose: #{choice}; Computer chose: #{computer_choice}"
+  prompt "You chose: #{choice}; Computer chose: #{computer_choice}\n\n"
 
   display_result(choice, computer_choice)
+  winner = return_winner(choice, computer_choice)
+  scoreboard[winner] += 1
+  display_score(scoreboard)
 
-  prompt "Do you want to play again?"
-  choice = gets.chomp
-
-  break unless choice.downcase.start_with?('y')
+  if scoreboard.has_value?(3)
+    overall = scoreboard.key(3)
+    prompt "The winner of the game is the #{overall.to_s}!"
+    break
+  end
 end
 
 prompt "Thank you for playing. Good bye!"
