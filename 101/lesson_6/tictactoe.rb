c1ed129d80aa6501d47267c086169e769c120383
Alex Sklar <sklarfox@gmail.com>
2022-10-06 13:35:24 -0600

    implimented scoreboard and rounds

--- 101/lesson_6/tictactoe.rb
+++ 101/lesson_6/tictactoe.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
+=begin
+  TODO items:
+  Display scoreboard
+  Indicate which squares are which number
+=end
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -44,7 +50,7 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square #{empty_squares(brd).join(', ')}"
+    prompt "Choose a square #{joinor(empty_squares(brd))}"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
@@ -101,32 +107,74 @@ def joinor(items, delimiter=', ', word='or')
   end
 end
 
-# Main Game Loop
-loop do
-  board = initialize_board
-  display_board(board)
+def initialize_scoreboard
+  scoreboard = {'Player' => 4, 'Computer' => 0, nil => 0}
+end
 
-  loop do
-    display_board(board)
+def display_scoreboard(scoreboard)
+  puts "Scoreboard".center(21)
+  puts "Player: #{scoreboard['Player']}, Computer: #{scoreboard['Computer']}"
+  prompt 'Enter to continue'
+  gets
+end
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-  end
-
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
-
+def play_again?
   prompt "Play again? (y or n)"
   answer = gets.chomp
-  break unless answer.downcase == 'y' || answer.downcase == 'yes'
+  answer.downcase == 'y' || answer.downcase == 'yes'
 end
+
+def overall_winner?(scoreboard)
+  scoreboard['Player'] == 5 || scoreboard['Computer'] == 5
+end
+
+def detect_overall_winner(scoreboard)
+  if scoreboard['Player'] == 5
+    'Player'
+  else
+    'Computer'
+  end
+end
+
+  # Game Loop
+  loop do
+    scoreboard = initialize_scoreboard
+
+    # Round Loop
+    loop do
+      board = initialize_board
+      display_board(board)
+
+      # Players place pieces loop
+      loop do
+        display_board(board)
+
+        player_places_piece!(board)
+        break if someone_won?(board) || board_full?(board)
+
+        computer_places_piece!(board)
+        break if someone_won?(board) || board_full?(board)
+      end
+
+      display_board(board)
+
+      # Add score if someone won
+      if someone_won?(board)
+        prompt "#{detect_winner(board)} won the round!"
+        scoreboard[detect_winner(board)] += 1
+      else
+        prompt "It's a tie!"
+      end
+
+      # Check for overall winner
+      if overall_winner?(scoreboard)
+        prompt "#{detect_overall_winner(scoreboard)} won the game!"
+        sleep 3
+        break
+      end
+      display_scoreboard(scoreboard)
+    end
+    break unless play_again?
+  end
 
 prompt "Thanks for playing Tic-tac-toe!"
