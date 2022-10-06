84de8f7895159e23c2937151a78d36188bd70bd0
Alex Sklar <sklarfox@gmail.com>
2022-10-05 19:33:06 -0600

    initialize and display board

--- 101/lesson_6/tictactoe.rb
+++ 101/lesson_6/tictactoe.rb
@@ -10,3 +10,29 @@
 9. If yes, go to #1
 10. Good bye!
 =end
+
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = ' '}
+  new_board
+end
+
+board = initialize_board
+
+display_board(board)
