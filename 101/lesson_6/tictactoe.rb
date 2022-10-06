2b4278d6e00d9aec0be30988a75a9c0498811073
Alex Sklar <sklarfox@gmail.com>
2022-10-05 19:55:07 -0600

    tictactoe player_piece and display board

--- 101/lesson_6/tictactoe.rb
+++ 101/lesson_6/tictactoe.rb
@@ -11,17 +11,40 @@
 10. Good bye!
 =end
 
-def display_board(brd)
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+def prompt(msg)
+ puts "=> #{msg}"
+end
+
+def display_board_DEPRICATED(brd) #TODO
   puts ""
-  puts "     |     |"
+  puts "1    | 2   |3"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
-  puts "     |     |"
+  puts "4    |5    |6"
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
   puts "     |     |"
   puts "-----+-----+-----"
+  puts "7    |8    |9"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
+  puts ""
+end
+
+def display_board(brd)
+  puts ""
+  puts "     |     | "
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     | "
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     | "
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
   puts ""
@@ -29,10 +52,30 @@ end
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' '}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select {|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square #{empty_squares(brd).join(", ")}"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+    sleep 1
+  end
+
+  brd[square] = PLAYER_MARKER
 end
 
 board = initialize_board
 
+display_board(board)
+
+player_places_piece!(board)
 display_board(board)
