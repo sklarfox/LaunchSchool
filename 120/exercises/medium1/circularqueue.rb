6bb891324f1e6e7d66802532b8cfbeb47ac72f96
Alex Sklar <sklarfox@gmail.com>
2022-12-01 14:50:22 -0800

    Initial problem breakdown with nouns and verbs

--- 120/exercises/medium1/circularqueue.rb
+++ 120/exercises/medium1/circularqueue.rb
@@ -1,32 +1,36 @@
+=begin
+ A circular queue is a collection of objects
+
+
+ Nouns:
+  Circular queue
+  Objects
+
+Verbs:
+  Enqueue - add an item to the queue, either in the position after the newest object, or overwrite the oldest object if full
+  Dequeue - remove and return the oldest item from the queue, or return nil if empty queue
+=end
+
 require 'pry'
 require 'pry-byebug'
 
 class CircularQueue
   def initialize(buffer_size)
     @buffer = Array.new(buffer_size)
-    @enqueue_cursor = 0
-    @dequeue_cursor = 0
   end
 
   def enqueue(element)
-    binding.pry
-    buffer[self.enqueue_cursor] = element
-    self.enqueue_cursor += 1
+
   end
 
   def dequeue
-    buffer[self.enqueue_cursor]
+
   end
-
-  # private
-
-  attr_accessor :buffer, :enqueue_cursor, :dequeue_cursor
-
 end
 
-queue = CircularQueue.new(3)
+# queue = CircularQueue.new(3)
 # puts queue.dequeue == nil
 
-queue.enqueue(1)
-queue.enqueue(2)
-puts queue.dequeue == 1
+# queue.enqueue(1)
+# queue.enqueue(2)
+# puts queue.dequeue == 1
