52be74f2c5064cd638744bf6f6cbb8032305b300
Alex Sklar <sklarfox@gmail.com>
2022-12-01 14:58:17 -0800

    Implemented QueueObject initial framework

--- 120/exercises/medium1/circularqueue.rb
+++ 120/exercises/medium1/circularqueue.rb
@@ -4,7 +4,7 @@
 
  Nouns:
   Circular queue
-  Objects
+  QueueObjects
 
 Verbs:
   Enqueue - add an item to the queue, either in the position after the newest object, or overwrite the oldest object if full
@@ -14,9 +14,22 @@ Verbs:
 require 'pry'
 require 'pry-byebug'
 
+class QueueObject
+  @@number_of_queue_objects = 0
+
+  attr_reader :ranking
+
+  def initialize(datatron2000)
+    @data = datatron2000
+    @ranking = @@number_of_queue_objects
+    @@number_of_queue_objects += 1
+  end
+end
+
 class CircularQueue
   def initialize(buffer_size)
     @buffer = Array.new(buffer_size)
+    binding.pry
   end
 
   def enqueue(element)
@@ -26,9 +39,19 @@ class CircularQueue
   def dequeue
 
   end
+
+  private
+
+  def oldest_object
+    @buffer.min { |a, b| a.ranking <=> b.ranking }
+  end
+
+  def newest_object
+
+  end
 end
 
-# queue = CircularQueue.new(3)
+queue = CircularQueue.new(3)
 # puts queue.dequeue == nil
 
 # queue.enqueue(1)
