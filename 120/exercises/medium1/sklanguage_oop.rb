947c24cd89577775aa5e96a45852ebefdb1d5100
Alex Sklar <sklarfox@gmail.com>
2022-12-06 15:09:52 -0800

    half refactored for further explanation

--- 120/exercises/medium1/sklanguage_oop.rb
+++ 120/exercises/medium1/sklanguage_oop.rb
@@ -29,26 +29,22 @@ class Minilang
   VALID_TOKENS = [:push, :add, :sub, :mult, :div, :mod, :pop, :print]
 
   def initialize(input)
-    @program = input.split
+    @program = input
     @register = 0
     @stack = Array.new
   end
 
-  def eval
-    begin
-      @program.each do |token|
-          if integer?(token)
-            update_register(token.to_i)
-          elsif !VALID_TOKENS.include?(token.downcase.to_sym)
-            @invalid = token
-            raise InvalidToken
-          else
-            self.send(token.downcase.to_sym)
-          end
+  def eval(input=nil)
+    input ? formatted = format(@program, input).split : formatted = @program.split
+    formatted.each do |token|
+        if integer?(token)
+          update_register(token.to_i)
+        elsif !VALID_TOKENS.include?(token.downcase.to_sym)
+          raise InvalidToken, "Invalid token:#{token}"
+        else
+          self.send(token.downcase.to_sym)
         end
-    rescue MinilangError
-      puts "Invalid Token: #{@invalid}"
-    end
+      end
   end
 
   def integer?(input)
@@ -84,6 +80,7 @@ class Minilang
   end
 
   def pop
+    raise EmptyStack, "Empty stack!" if @stack.empty?
     @register = @stack.pop
   end
 
@@ -97,35 +94,63 @@ class Minilang
   end
 end
 
-# Minilang.new('PRINT').eval
+Minilang.new('PRINT').eval
 # 0
 
-# Minilang.new('5 PUSH 3 MULT PRINT').eval
+Minilang.new('5 PUSH 3 MULT PRINT').eval
 # 15
 
-# Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
 # 5
 # 3
 # 8
 
-# Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
 # 10
 # 5
 
 # Minilang.new('5 PUSH POP POP PRINT').eval
-# Empty stack!
+# # Empty stack!
 
-# Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
 # 6
 
-# Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
 # 12
 
-# Minilang.new('-3 PUSH 5 XSUB PRINT').eval
+Minilang.new('-3 PUSH 5 XSUB PRINT').eval
 # Invalid token: XSUB
 
-# Minilang.new('-3 PUSH 5 SUB PRINT').eval
+Minilang.new('-3 PUSH 5 SUB PRINT').eval
 # 8
 
-# Minilang.new('6 PUSH').eval
+Minilang.new('6 PUSH').eval
 # (nothing printed; no PRINT commands)
+
+# CENTIGRADE_TO_FAHRENHEIT =
+#   '5 PUSH %<degrees_c>d PUSH 9 MULT DIV PUSH 32 ADD PRINT'
+# minilang = Minilang.new(CENTIGRADE_TO_FAHRENHEIT)
+# minilang.eval(degrees_c: 100)
+# # 212
+# minilang.eval(degrees_c: 0)
+# # 32
+# minilang.eval(degrees_c: -40)
+# -40
+
+# KILO * 3/5 = MILES
+# 
+
+
+# FAHRENHEIT_TO_CENTIGRADE = "9 PUSH 5 PUSH 32 PUSH %<degrees_f>d SUB MULT DIV PRINT"
+# minilang = Minilang.new(FAHRENHEIT_TO_CENTIGRADE)
+# minilang.eval(degrees_f: 212)
+# minilang.eval(degrees_f: 32)
+# minilang.eval(degrees_f: -40)
+
+# KPH_TO_MPH = "5 PUSH 3 PUSH %<kph>s MULT DIV PRINT"
+# minilang = Minilang.new(KPH_TO_MPH)
+# minilang.eval(kph: 100)
+
+RECTANGLE_AREA = "%<side1>d PUSH %<side2>d MULT PRINT"
+minilang = Minilang.new(RECTANGLE_AREA)
+minilang.eval({side1: 20, side2: 2})
